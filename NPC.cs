982469b657d90698a5abc05baab52f2690b13888
rejefijejrpe2 <126339333+rejefijejrpe2@users.noreply.github.com>
2023-11-12 17:26:15 +0500

    npc
    npc

---
+++ NPC.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    //здоровье
+    public int health = 5;
+    //уровень
+    public int level = 1;
+    //скорость
+    public float speed = 1.2f;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
